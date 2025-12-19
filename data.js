a34dfdce0aba58e39d32b82196892a55bfcbba66
ahnl236-droid <ahnl236@newschool.edu>
2025-12-19 17:15:16 -0500

    Implement appendData function to display data
    Added a function to append data to the main container.

--- data.js
+++ data.js
@@ -11,3 +11,12 @@ fetch('https://api.thecatapi.com/v1/images/search?limit=10').then(function (resp
 	// There was an error
 	console.warn('Something went wrong.', err);
 });
+
+function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].id + ' pic: ' + data[i].url + `<div class="image-list"><img src ="${data[i].url}"</div>`;
+        mainContainer.appendChild(div);
+    }
+}
